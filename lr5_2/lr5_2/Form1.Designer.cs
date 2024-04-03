9dc1637fd94b30c6ab1427b5453306540c54f3b7
Tk22309 <129153902+Tk22309@users.noreply.github.com>
2024-04-03 22:10:02 +0300

    Add files via upload

---
+++ lr5_2/lr5_2/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace YourNamespace
+{
+    partial class YourForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MyrichTextBox = new System.Windows.Forms.RichTextBox();
+            this.listBoxRes1 = new System.Windows.Forms.ListBox();
+            this.listBoxRes2 = new System.Windows.Forms.ListBox();
+            this.ButtonCalculate = new System.Windows.Forms.Button();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // MyrichTextBox
+            // 
+            this.MyrichTextBox.Location = new System.Drawing.Point(63, 30);
+            this.MyrichTextBox.Name = "MyrichTextBox";
+            this.MyrichTextBox.Size = new System.Drawing.Size(190, 134);
+            this.MyrichTextBox.TabIndex = 0;
+            this.MyrichTextBox.Text = "";
+            // 
+            // listBoxRes1
+            // 
+            this.listBoxRes1.FormattingEnabled = true;
+            this.listBoxRes1.Location = new System.Drawing.Point(398, 41);
+            this.listBoxRes1.Name = "listBoxRes1";
+            this.listBoxRes1.Size = new System.Drawing.Size(268, 134);
+            this.listBoxRes1.TabIndex = 1;
+            // 
+            // listBoxRes2
+            // 
+            this.listBoxRes2.FormattingEnabled = true;
+            this.listBoxRes2.Location = new System.Drawing.Point(398, 201);
+            this.listBoxRes2.Name = "listBoxRes2";
+            this.listBoxRes2.Size = new System.Drawing.Size(268, 134);
+            this.listBoxRes2.TabIndex = 2;
+            // 
+            // ButtonCalculate
+            // 
+            this.ButtonCalculate.Location = new System.Drawing.Point(63, 208);
+            this.ButtonCalculate.Name = "ButtonCalculate";
+            this.ButtonCalculate.Size = new System.Drawing.Size(160, 47);
+            this.ButtonCalculate.TabIndex = 3;
+            this.ButtonCalculate.Text = "Calculate";
+            this.ButtonCalculate.UseVisualStyleBackColor = true;
+            this.ButtonCalculate.Click += new System.EventHandler(this.ButtonCalculate_Click_1);
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Location = new System.Drawing.Point(62, 282);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(160, 20);
+            this.textBoxN.TabIndex = 4;
+            // 
+            // YourForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxN);
+            this.Controls.Add(this.ButtonCalculate);
+            this.Controls.Add(this.listBoxRes2);
+            this.Controls.Add(this.listBoxRes1);
+            this.Controls.Add(this.MyrichTextBox);
+            this.Name = "YourForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox MyrichTextBox;
+        private System.Windows.Forms.ListBox listBoxRes1;
+        private System.Windows.Forms.ListBox listBoxRes2;
+        private System.Windows.Forms.Button ButtonCalculate;
+        private System.Windows.Forms.TextBox textBoxN;
+    }
+}
+
---
+++ lr5_2/lr5_2/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace YourNamespace
+{
+    public partial class YourForm : Form
+    {
+        public YourForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateDuplicates(string[] lines)
+        {
+            // Задача а)
+            var duplicatesCount = new Dictionary<string, int>();
+
+            foreach (var line in lines)
+            {
+                if (duplicatesCount.ContainsKey(line))
+                {
+                    duplicatesCount[line]++;
+                }
+                else
+                {
+                    duplicatesCount[line] = 1;
+                }
+            }
+
+            // Вивести результати в listBoxRes1
+            listBoxRes1.Items.Clear();
+            foreach (var pair in duplicatesCount)
+            {
+                listBoxRes1.Items.Add($"{pair.Key}: {pair.Value}");
+            }
+        }
+
+        private void CalculateStartsWith(string[] lines)
+        {
+            // Задача б)
+            int n = Convert.ToInt32(textBoxN.Text);
+
+            var startsWithCount = new Dictionary<string, int>();
+
+            foreach (var line in lines)
+            {
+                string prefix = line.Length >= n ? line.Substring(0, n) : line;
+
+                if (startsWithCount.ContainsKey(prefix))
+                {
+                    startsWithCount[prefix]++;
+                }
+                else
+                {
+                    startsWithCount[prefix] = 1;
+                }
+            }
+
+            // Вивести результати в listBoxRes2
+            listBoxRes2.Items.Clear();
+            foreach (var pair in startsWithCount)
+            {
+                listBoxRes2.Items.Add($"{pair.Key}: {pair.Value}");
+            }
+        }
+
+        private void ButtonCalculate_Click_1(object sender, EventArgs e)
+        {
+            // Отримати рядки з MyRichTextBox
+            string[] lines = MyrichTextBox.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Виконати обрахунки
+            CalculateDuplicates(lines);
+            CalculateStartsWith(lines);
+
+        }
+    }
+}
